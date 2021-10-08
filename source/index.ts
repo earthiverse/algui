86a8bb224f6600ca300e60272d89fbf993545292
Kent Rasmussen <hyprkookeez@gmail.com>
2021-10-08 10:05:53 +0900

    Add eslint, separate logic

--- source/index.ts
+++ source/index.ts
@@ -1,50 +1,50 @@
-import * as PIXI from 'pixi.js'
+import * as PIXI from "pixi.js"
 import { SpatialHash } from "pixi-cull"
-import { Viewport } from 'pixi-viewport'
+import { Viewport } from "pixi-viewport"
 import "./index.css"
 import { MapName } from "alclient"
-import { renderMap } from './map'
+import { renderMap } from "./map"
 
 // These settings make PIXI work well for pixel art based games
-PIXI.settings.ROUND_PIXELS = true;
+PIXI.settings.ROUND_PIXELS = true
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST
 
 // Add the view to the DOM
 const app = new PIXI.Application()
 document.body.appendChild(app.view)
 const viewport = new Viewport({
-  screenWidth: window.innerWidth,
-  screenHeight: window.innerHeight,
-  interaction: app.renderer.plugins.interaction
+    interaction: app.renderer.plugins.interaction,
+    screenHeight: window.innerHeight,
+    screenWidth: window.innerWidth
 })
 viewport.pinch().drag().decelerate()
 app.stage.addChild(viewport)
 
 // Add culling to improve performance
 const cull = new SpatialHash({
-  dirtyTest: true,
-  simpleTest: true
+    dirtyTest: true,
+    simpleTest: true
 })
 cull.cull(viewport.getVisibleBounds())
 PIXI.Ticker.shared.add(() => {
-  if (viewport.dirty) {
-    cull.cull(viewport.getVisibleBounds())
-    viewport.dirty = false
-  }
+    if (viewport.dirty) {
+        cull.cull(viewport.getVisibleBounds())
+        viewport.dirty = false
+    }
 })
 
 // Fill the screen, and keep it full on window resize
-window.addEventListener('resize', resize);
-function resize() { 
-  app.renderer.resize(window.innerWidth, window.innerHeight)
-  viewport.resize(window.innerWidth, window.innerHeight)
+window.addEventListener("resize", resize)
+function resize() {
+    app.renderer.resize(window.innerWidth, window.innerHeight)
+    viewport.resize(window.innerWidth, window.innerHeight)
 }
 resize()
 
 // Render a map!
-const map : MapName = "winterland"
+const map: MapName = "winterland"
 renderMap(viewport, cull, map)
 viewport.moveCenter(0, 0)
 viewport.animate({
-  scale: 2
+    scale: 2
 })
--- source/map.ts
+++ source/map.ts
@@ -1,19 +1,19 @@
-import * as PIXI from 'pixi.js'
+import * as PIXI from "pixi.js"
 import { SpatialHash } from "pixi-cull"
-import { Viewport } from 'pixi-viewport'
-import { GData, GGeometry, MapName } from 'alclient'
-import { getTexture } from './texture'
-const G:GData = require("./G.json")
+import { Viewport } from "pixi-viewport"
+import { GData, GGeometry, MapName } from "alclient"
+import { getTexture } from "./texture"
+import G from "./G.json"
 
-export function renderMap(viewport : Viewport, cull: SpatialHash, map: MapName) {
-    const geometry:GGeometry = G.geometry[map as MapName]
+export function renderMap(viewport: Viewport, cull: SpatialHash, map: MapName): void {
+    const geometry: GGeometry = (G as GData).geometry[map as MapName]
 
     const base = new PIXI.Container()
     base.interactiveChildren = false
     base.zIndex = -1
     viewport.addChild(base)
     // Draw default layer
-    if(geometry.default) {
+    if (geometry.default) {
         const texture = getTexture(map, geometry.default)
         const tile = new PIXI.TilingSprite(texture, geometry.max_x - geometry.min_x + texture.width, geometry.max_y - geometry.min_y + texture.height)
         tile.x = geometry.min_x
@@ -22,30 +22,30 @@ export function renderMap(viewport : Viewport, cull: SpatialHash, map: MapName) 
     }
 
     // Draw placements
-    if(geometry.placements) {
-    for(let [index, x1, y1, x2, y2] of geometry.placements) {
-        const texture = getTexture(map, index)
+    if (geometry.placements) {
+        for (const [index, x1, y1, x2, y2] of geometry.placements) {
+            const texture = getTexture(map, index)
 
-        if(x2 != undefined) {
-            for(let x = x1; x <= x2; x+=texture.width) {
-                for(let y = y1; y <= y2; y+=texture.height) {
+            if (x2 != undefined) {
+                for (let x = x1; x <= x2; x += texture.width) {
+                    for (let y = y1; y <= y2; y += texture.height) {
                     // Tiling Sprite
-                    const tile = new PIXI.Sprite(texture);
-                    tile.x = x
-                    tile.y = y
-                    tile.width = texture.width
-                    tile.height = texture.height
-                    base.addChild(tile)
+                        const tile = new PIXI.Sprite(texture)
+                        tile.x = x
+                        tile.y = y
+                        tile.width = texture.width
+                        tile.height = texture.height
+                        base.addChild(tile)
+                    }
                 }
-            }
-        } else {
+            } else {
             // Single Sprite
-            const tile = new PIXI.Sprite(texture);
-            tile.x = x1
-            tile.y = y1
-            tile.width = texture.width
-            tile.height = texture.height
-            base.addChild(tile)
+                const tile = new PIXI.Sprite(texture)
+                tile.x = x1
+                tile.y = y1
+                tile.width = texture.width
+                tile.height = texture.height
+                base.addChild(tile)
             }
         }
     }
@@ -56,33 +56,33 @@ export function renderMap(viewport : Viewport, cull: SpatialHash, map: MapName) 
     top.interactiveChildren = false
     top.zIndex = 1
     viewport.addChild(top)
-    if(geometry.groups) {
-    for(let group of geometry.groups) {
-        for(let [index, x1, y1, x2, y2, yDisp] of group) {
-        const texture = getTexture(map, index)
-        if(x2 != undefined) {
-            for(let x = x1; x <= x2; x+=texture.width) {
-                for(let y = y1; y <= y2; y+=texture.height) {
-                    // Tiling Sprite
-                    const tile = new PIXI.Sprite(texture);
-                    tile.x = x
-                    tile.y = y
+    if (geometry.groups) {
+        for (const group of geometry.groups) {
+            for (const [index, x1, y1, x2, y2] of group) {
+                const texture = getTexture(map, index)
+                if (x2 != undefined) {
+                    for (let x = x1; x <= x2; x += texture.width) {
+                        for (let y = y1; y <= y2; y += texture.height) {
+                        // Tiling Sprite
+                            const tile = new PIXI.Sprite(texture)
+                            tile.x = x
+                            tile.y = y
+                            tile.width = texture.width
+                            tile.height = texture.height
+                            top.addChild(tile)
+                        }
+                    }
+                } else {
+                // Single Sprite
+                    const tile = new PIXI.Sprite(texture)
+                    tile.x = x1
+                    tile.y = y1
                     tile.width = texture.width
                     tile.height = texture.height
                     top.addChild(tile)
                 }
             }
-        } else {
-            // Single Sprite
-            const tile = new PIXI.Sprite(texture);
-            tile.x = x1
-            tile.y = y1
-            tile.width = texture.width
-            tile.height = texture.height
-            top.addChild(tile)
         }
-        }
-    }
     }
     cull.addContainer(top, true)
 }
